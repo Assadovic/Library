f96cd3e3ec53d805b72c918d264fcae5177e7423
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-07-02 21:23:26 +0900

    Fix
    gccのオプションを変更してコンパイルし直す。

--- Common/C++/builds_x64.sh
+++ Common/C++/builds_x64.sh
@@ -2,8 +2,8 @@
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-export CFLAGS="-shared -m64 -fPIC -Wall -O2 -pipe -D UNIX"
-export CXXFLAGS="-shared -m64 -fPIC -Wall -O2 -pipe -D UNIX" 
+export CFLAGS="-shared -m64 -fPIC -Wall -O3 -pipe -D UNIX"
+export CXXFLAGS="-shared -m64 -fPIC -Wall -O3 -pipe -D UNIX" 
 export BUILD="../build/x64"
 
 cd ${cwd}/Library/Library
--- Common/C++/builds_x86.sh
+++ Common/C++/builds_x86.sh
@@ -2,8 +2,8 @@
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-export CFLAGS="-shared -m32 -mfpmath=sse -march=pentium4 -fPIC -Wall -O2 -pipe -D UNIX"
-export CXXFLAGS="-shared -m32 -mfpmath=sse -march=pentium4 -fPIC -Wall -O2 -pipe -D UNIX"
+export CFLAGS="-shared -m32 -mfpmath=sse -march=pentium4 -fPIC -Wall -O3 -pipe -D UNIX"
+export CXXFLAGS="-shared -m32 -mfpmath=sse -march=pentium4 -fPIC -Wall -O3 -pipe -D UNIX"
 export BUILD="../build/x86"
 
 cd ${cwd}/Library/Library
