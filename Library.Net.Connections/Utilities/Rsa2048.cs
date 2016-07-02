d07139edff81882f9ddbc8ab1deddd4a2d3ae68d
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-07-02 18:20:40 +0900

    Fix
    Mono環境のマルチスレッド下でRSACryptoServiceProviderを使用したときに問題が起こるバグに対処。

--- Library.Net.Connections/Utilities/Rsa2048.cs
+++ Library.Net.Connections/Utilities/Rsa2048.cs
@@ -5,31 +5,74 @@ namespace Library.Net.Connections
 {
     static class Rsa2048
     {
+#if Unix
+        private static object _lockObject = "RSACryptoServiceProvider";
+#endif
+
         public static void CreateKeys(out byte[] publicKey, out byte[] privateKey)
         {
+#if Windows
             using (var rsa = new RSACryptoServiceProvider(2048))
             {
                 publicKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(false));
                 privateKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(true));
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                using (var rsa = new RSACryptoServiceProvider(2048))
+                {
+                    publicKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(false));
+                    privateKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(true));
+                }
+            }
+#endif
         }
 
         public static byte[] Encrypt(byte[] publicKey, byte[] value)
         {
+#if Windows
             using (var rsa = new RSACryptoServiceProvider())
             {
                 rsa.FromXmlString(Encoding.ASCII.GetString(publicKey));
                 return rsa.Encrypt(value, true);
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(Encoding.ASCII.GetString(publicKey));
+                    return rsa.Encrypt(value, true);
+                }
+            }
+#endif
         }
 
         public static byte[] Decrypt(byte[] privateKey, byte[] value)
         {
+#if Windows
             using (var rsa = new RSACryptoServiceProvider())
             {
                 rsa.FromXmlString(Encoding.ASCII.GetString(privateKey));
                 return rsa.Decrypt(value, true);
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(Encoding.ASCII.GetString(privateKey));
+                    return rsa.Decrypt(value, true);
+                }
+            }
+#endif
         }
     }
 }
--- Library.Security/Utilities/Rsa2048_Sha512.cs
+++ Library.Security/Utilities/Rsa2048_Sha512.cs
@@ -7,17 +7,35 @@ namespace Library.Security
 {
     class Rsa2048_Sha256
     {
+#if Unix
+        private static object _lockObject = "RSACryptoServiceProvider";
+#endif
+
         public static void CreateKeys(out byte[] publicKey, out byte[] privateKey)
         {
+#if Windows
             using (var rsa = new RSACryptoServiceProvider(2048))
             {
                 publicKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(false));
                 privateKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(true));
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                using (var rsa = new RSACryptoServiceProvider(2048))
+                {
+                    publicKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(false));
+                    privateKey = Encoding.ASCII.GetBytes(rsa.ToXmlString(true));
+                }
+            }
+#endif
         }
 
         public static byte[] Sign(byte[] privateKey, Stream stream)
         {
+#if Windows
             using (var rsa = new RSACryptoServiceProvider())
             {
                 rsa.FromXmlString(Encoding.ASCII.GetString(privateKey));
@@ -30,10 +48,30 @@ namespace Library.Security
                     return rsaFormatter.CreateSignature(Sha256.ComputeHash(stream));
                 }
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(Encoding.ASCII.GetString(privateKey));
+
+                    var rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
+                    rsaFormatter.SetHashAlgorithm("SHA256");
+
+                    using (var Sha256 = SHA256.Create())
+                    {
+                        return rsaFormatter.CreateSignature(Sha256.ComputeHash(stream));
+                    }
+                }
+            }
+#endif
         }
 
         public static bool Verify(byte[] publicKey, byte[] signature, Stream stream)
         {
+#if Windows
             try
             {
                 using (var rsa = new RSACryptoServiceProvider())
@@ -53,6 +91,32 @@ namespace Library.Security
             {
                 return false;
             }
+#endif
+
+#if Unix
+            lock (_lockObject)
+            {
+                try
+                {
+                    using (var rsa = new RSACryptoServiceProvider())
+                    {
+                        rsa.FromXmlString(Encoding.ASCII.GetString(publicKey));
+
+                        var rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                        rsaDeformatter.SetHashAlgorithm("SHA256");
+
+                        using (var Sha256 = SHA256.Create())
+                        {
+                            return rsaDeformatter.VerifySignature(Sha256.ComputeHash(stream), signature);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+#endif
         }
     }
 }
